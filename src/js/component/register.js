4cfd7fe48052e02187b4a7c267c44a803f6fe0ee
Laura-Romero <laura90romero@gmail.com>
2020-09-13 22:44:29 +0000

    doing validations

--- src/js/component/register.js
+++ src/js/component/register.js
@@ -1,61 +1,137 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
 export const Register = () => {
+	const { actions, store } = useContext(Context);
+
+	const [inputFullName, setFullName] = useState("");
+	const [inputUsername, setUserName] = useState("");
+	const [inputPassw, setPassw] = useState("");
+	const [inputMail, setMail] = useState("");
+	const [inputCountry, setCountry] = useState("");
+	const [inputCity, setCity] = useState("");
+
+	let validation = (username, email, fullName, country, city) => {
+		const checkEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		const checkCharacter = /[&\/\\#,+()$~%.'":*?<>{}]/g;
+
+		if (!checkEmail.test(email)) {
+			console.log(checkEmail.test(email));
+			setMail("Error");
+		} else {
+			console.log(checkEmail.test(email), "diferent");
+		}
+
+		if (username.length < 5) {
+			setUserName("Error");
+		}
+
+		if (checkCharacter.test(fullName) || fullName == "") {
+			setFullName("Error");
+		}
+
+		if (checkCharacter.test(country) || country == "") {
+			setCountry("Error");
+		}
+
+		if (checkCharacter.test(city) || city == "") {
+			setCity("Error");
+		}
+	};
+
 	return (
-		<div className=" container d-flex flex-column justify-content-end">
-			<div className="row d-flex justify-content-center">
-				<div className="col-10 col-lg-4 register border rounded ">
-					<form action="" method="put">
-						<div className="row title d-flex justify-content-center py-3">
-							<h3>
-								<strong>Registration</strong>
-							</h3>
-						</div>
-
-						<div className="row mx-1">
-							<label>Full name:</label>
-						</div>
-						<div className="row mx-1">
-							<input type="text" className="form-control" />
-						</div>
-
-						<div className="row mx-1 pt-3">
-							<label>Username:</label>
-						</div>
-						<div className="row mx-1">
-							<input type="text" className="form-control" />
-						</div>
-
-						<div className="row mx-1 pt-3">
-							<label>Password:</label>
-						</div>
-						<div className="row mx-1">
-							<input type="password" className="form-control" />
-						</div>
-
-						<div className="row d-flex justify-content-between pt-3">
-							<div className="col ml-2">
-								<label>Country:</label>
-							</div>
-							<div className="col">
-								<label>City:</label>
-							</div>
-						</div>
-						<div className="row d-flex justify-content-between">
-							<div className="col ml-1">
-								<input type="text" className="form-control" />
-							</div>
-							<div className="col mr-1">
-								<input type="text" className="form-control" />
-							</div>
-						</div>
-
-						<div className="row mx-1 pb-4">
-							<button className="btn btn-block mt-3">SUBMIT</button>
-						</div>
-					</form>
+		<div className="row d-flex justify-content-center">
+			<form className="register col-10 col-lg-4 d-flex flex-column" action="" method="post">
+				<div className="row title d-flex justify-content-center py-3">
+					<h3>
+						<strong>Registration</strong>
+					</h3>
 				</div>
-			</div>
+
+				<div className="row mx-1">
+					<label>Full name:</label>
+				</div>
+				<div className="row mx-1">
+					<input
+						type="text"
+						onChange={e => setFullName(e.target.value)}
+						className={inputFullName == "Error" ? "form-control border border-danger" : "form-control"}
+					/>
+				</div>
+
+				<div className="row mx-1 pt-3">
+					<label>Username:</label>
+				</div>
+				<div className="row mx-1">
+					<input
+						type="text"
+						onChange={e => setUserName(e.target.value)}
+						className={inputUsername == "Error" ? "form-control border border-danger" : "form-control"}
+					/>
+					{/* <div className="invalid-feedback">More of 5 Characters</div> */}
+				</div>
+
+				<div className="row mx-1 pt-3">
+					<label>Password:</label>
+				</div>
+				<div className="row mx-1">
+					<input type="password" onChange={e => setPassw(e.target.value)} className="form-control" />
+				</div>
+
+				<div className="row mx-1 pt-3">
+					<label>Email addres:</label>
+				</div>
+				<div className="row mx-1">
+					<input
+						type="text"
+						onChange={e => setMail(e.target.value)}
+						className={inputMail == "Error" ? "form-control border border-danger" : "form-control"}
+					/>
+				</div>
+
+				<div className="row d-flex justify-content-between pt-3">
+					<div className="col ml-2">
+						<label>Country:</label>
+					</div>
+					<div className="col">
+						<label>City:</label>
+					</div>
+				</div>
+				<div className="row d-flex justify-content-between">
+					<div className="col ml-1">
+						<input
+							type="text"
+							onChange={e => setCountry(e.target.value)}
+							className={inputCountry == "Error" ? "form-control border border-danger" : "form-control"}
+						/>
+					</div>
+					<div className="col mr-1">
+						<input
+							type="text"
+							onChange={e => setCity(e.target.value)}
+							className={inputCity == "Error" ? "form-control border border-danger" : "form-control"}
+						/>
+					</div>
+				</div>
+
+				<div className="row mx-1 pb-4">
+					<button
+						className="btn btn-block mt-3"
+						onClick={e => {
+							e.preventDefault();
+							validation(inputUsername, inputMail, inputFullName, inputCountry, inputCity);
+							actions.addUser(
+								inputFullName,
+								inputUsername,
+								inputPassw,
+								inputMail,
+								inputCountry,
+								inputCity
+							);
+						}}>
+						SUBMIT
+					</button>
+				</div>
+			</form>
 		</div>
 	);
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			arrayNewUser: [],
 			demo: [
 				{
 					title: "FIRST",
@@ -15,29 +16,29 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			addUser: (inputFullName, inputUsername, inputPassw, inputMail, inputCountry, inputCity) => {
+				fetch("https://3000-b7899868-7c51-4238-bf71-3b89fc0786f9.ws-eu01.gitpod.io/users", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						fullname: inputFullName,
+						username: inputUsername,
+						password: inputPassw,
+						email: inputMail,
+						country: inputCountry,
+						city: inputCity,
+						is_active: true
+					})
+				})
+					.then(function(response) {
+						return response.json();
+					})
+					.then(function(responseAsJson) {
+						console.log(responseAsJson);
+					});
 			}
+
+			// Use getActions to call a function within a fuction
 		}
 	};
 };
