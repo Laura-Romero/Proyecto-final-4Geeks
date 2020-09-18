df97c8a046639752ea5a4a5984306f630cc184d9
Laura-Romero <laura90romero@gmail.com>
2020-09-18 09:54:55 +0000

    ....

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -17,25 +17,39 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			addUser: (inputFullName, inputUsername, inputPassw, inputMail, inputCountry, inputCity) => {
-				fetch("https://3000-b7899868-7c51-4238-bf71-3b89fc0786f9.ws-eu01.gitpod.io/users", {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify({
-						fullname: inputFullName,
-						username: inputUsername,
-						password: inputPassw,
-						email: inputMail,
-						country: inputCountry,
-						city: inputCity,
-						is_active: true
+				if (
+					(inputFullName,
+					inputUsername,
+					inputPassw,
+					inputMail,
+					inputCountry,
+					inputCity != "Error" || inputFullName,
+					inputUsername,
+					inputPassw,
+					inputMail,
+					inputCountry,
+					inputCity != "")
+				) {
+					fetch("https://3000-a3b82b6f-7895-458b-b9a0-984cf8e6081d.ws-eu01.gitpod.io/user", {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							fullname: inputFullName,
+							username: inputUsername,
+							password: inputPassw,
+							email: inputMail,
+							country: inputCountry,
+							city: inputCity,
+							is_active: true
+						})
 					})
-				})
-					.then(function(response) {
-						return response.json();
-					})
-					.then(function(responseAsJson) {
-						console.log(responseAsJson);
-					});
+						.then(function(response) {
+							return response.json();
+						})
+						.then(function(responseAsJson) {
+							console.log(responseAsJson);
+						});
+				}
 			}
 
 			// Use getActions to call a function within a fuction
