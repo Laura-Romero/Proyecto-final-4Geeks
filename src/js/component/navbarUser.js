7a1ff78a1e12ba2fc32132a96782abd91c814df6
JuanLGC <juangcardinale@gmail.com>
2020-09-24 10:46:10 +0000

    final touch 2

--- src/js/component/navbarUser.js
+++ src/js/component/navbarUser.js
@@ -6,7 +6,6 @@ import "../../styles/navbarUser.scss";
 export const NavbarUser = props => {
 	return (
 		<div className="category-navbar navbar navbar--user user-navbar--background-color d-flex">
-			<div />
 			<Link to="/" className="m-0 nav-item user-navbar__button d-flex">
 				<i className="fa fa-sign-out ml-1 p-1" aria-hidden="true" />
 			</Link>
